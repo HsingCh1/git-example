4e50ba55728bc822d92e4f6c438c5a9954bc85a7
HsingCh1 <conny071297@gmail>
2020-03-13 08:01:10 -0700

    modified eg_a

--- eg_a.c
+++ eg_a.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-
 	printf("this is an example\n");
+
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 22; j++){
+			if(j >= 9 - i && j < 10) printf("#");
+			else if (j >= 12 && j < i + 13) printf("#");
+			else printf(" ");
+		}
+		printf("\n");
+	}
 }
